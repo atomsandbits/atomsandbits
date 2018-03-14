47ae93dd3fb8364685c38c75f48445ae7c143efd
Jordan Garside <jordangarside@gmail.com>
2018-03-14 11:13:00 -0400

    resultsFeed redirect fix (adds background polling)

--- services/webapp/client/imports/pages/ResultsFeed/withData/index.js
+++ services/webapp/client/imports/pages/ResultsFeed/withData/index.js
@@ -1,40 +1,57 @@
+import { Meteor } from 'meteor/meteor';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { compose } from 'recompose';
 
-const withData = compose(
-  graphql(
-    gql`
-      query {
-        results {
-          type
-          createdAt
-          project {
-            id
-          }
-          calculation {
-            id
-            createdAt
-            completed
-            properties {
-              energy
-              force
-              geometries
-            }
-            geometries {
-              id
-              molecularFormula
-              mediumImage
-            }
-          }
+import apolloClient from '/client/imports/apollo-client';
+
+const dataQuery = gql`
+  query {
+    results {
+      type
+      createdAt
+      project {
+        id
+      }
+      calculation {
+        id
+        createdAt
+        completed
+        properties {
+          energy
+          force
+          geometries
+        }
+        geometries {
+          id
+          molecularFormula
+          mediumImage
         }
       }
-    `,
-    {
-      options: () => ({ pollInterval: 3000 }),
     }
-  )
+  }
+`;
+
+const withData = compose(
+  graphql(dataQuery, {
+    options: () => ({ pollInterval: 60000 }),
+  })
 );
+
+const queryWatcher = apolloClient.watchQuery({
+  query: dataQuery,
+  fetchPolicy: 'network-only',
+});
+let loginTracker;
+Meteor.startup(() => {
+  loginTracker = Tracker.autorun(() => {
+    if (Meteor.userId()) {
+      queryWatcher.startPolling(5000);
+    } else {
+      queryWatcher.stopPolling();
+    }
+  });
+});
 
 export { withData };
 export default { withData };
