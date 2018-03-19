c91ddaf8e2460747d3d2f3ff3e26fa002bd242cc
John Parkhill <john.parkhill@gmail.com>
2018-03-18 21:21:04 -0400

    Analytic Harmonic Spectra with current TensorMol

--- services/tensormol/calculations/harmonic_spectra.py
+++ services/tensormol/calculations/harmonic_spectra.py
@@ -5,7 +5,13 @@ import numpy as np
 def main(manager, molecule):
     """main."""
     m = molecule
-
+    def GetEnergyForceHess(m):
+        def hess(x_):
+            tmpm = Mol(m.atoms,x_)
+            Energy, Force, Hessian = manager.EvalBPDirectEEHessSingle(tmpm, PARAMS["AN1_r_Rc"], PARAMS["AN1_a_Rc"], PARAMS["EECutoffOff"])
+            return Energy[0],Force[0],Hessian[0].reshape((3*m.NAtoms(),3*m.NAtoms()))
+        return hess
+    EFH = GetEnergyForceHess(m)
     def EnAndForce(x_, DoForce=False):
         """Calculate energy and force."""
         mtmp = Mol(molecule.atoms, x_)
@@ -19,7 +25,6 @@ def main(manager, molecule):
             return energy, force
         else:
             return energy
-
     def ChargeField(x_):
         m.coords = x_
         Etotal, Ebp, Ebp_atom, Ecc, Evdw, mol_dipole, atom_charge, gradient = manager.EvalBPDirectEEUpdateSingle(
@@ -28,7 +33,6 @@ def main(manager, molecule):
         energy = Etotal[0]
         force = gradient[0]
         return atom_charge[0]
-
     def DipoleField(x_):
         q = np.asarray(ChargeField(x_))
         dipole = np.zeros(3)
@@ -46,9 +50,9 @@ def main(manager, molecule):
         molecule.coords,
         molecule.atoms,
         WriteNM_=True,
-        Mu_=DipoleField)
-    return molecule, w, i
-
+        Mu_=DipoleField,
+		h_ = lambda x: EFH(x)[2])
+    return molecule, w, i, TD
 
 if __name__ == "__main__":
     import os
