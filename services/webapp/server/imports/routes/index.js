8338dc94a77c987991dac2bd89d5f12a4b4ce273
Jordan Garside <jordangarside@gmail.com>
2018-04-15 18:03:11 -0400

    return 404 when geometry not found

--- services/webapp/server/imports/routes/index.js
+++ services/webapp/server/imports/routes/index.js
@@ -3,6 +3,11 @@ import { Geometries } from '/server/imports/db';
 Picker.route('/geometry/:_id/image/medium', function(params, req, res, next) {
   const { _id } = params;
   const geometry = Geometries.findOne(_id, { fields: { 'images.512': 1 } });
+  if (!geometry) {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
   const b64str = geometry.images
     ? geometry.images['512'].split('data:image/png;base64,')[1]
     : null;
