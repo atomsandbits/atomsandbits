35698e9dc150fcabaa337b7d16ae86373e296a65
Jordan Garside <jordangarside@gmail.com>
2018-05-19 23:09:40 -0400

    fix geometry page import

--- services/webapp/client/imports/components/Expandable/index.js
+++ services/webapp/client/imports/components/Expandable/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { compose, withState } from 'recompose';
-import {
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-} from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import {
