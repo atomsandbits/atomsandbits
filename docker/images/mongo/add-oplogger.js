5ea92a35efc9d72a36bab9fcede82bdd73090dbc
Jordan Garside <jordangarside@gmail.com>
2018-06-16 22:00:34 -0400

    update mongo version for docker image

--- docker/images/mongo/add-oplogger.js
+++ docker/images/mongo/add-oplogger.js
@@ -1,4 +1,4 @@
 use admin
 db.createUser({user: "$OPLOG_USER", pwd: "$OPLOG_PASSWORD", roles: []})
 db.runCommand({ createRole: "oplogger", privileges: [{ resource: { db: 'local', collection: 'system.replset'}, actions: ['find']}], roles: [{role: 'read', db: 'local'}] })
-db.adminCommand({setFeatureCompatibilityVersion: "3.4"})
+db.adminCommand({setFeatureCompatibilityVersion: "3.6.4"})
